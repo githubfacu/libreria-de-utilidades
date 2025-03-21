a828f4227dcb8387d672b85209748b035cc4c0da
githubfacu <elorzfacundo@gmail.com>
2025-03-21 16:35:19 -0300

    inicialización de proyecto

---
+++ src/check/isEmpty.ts
@@ -0,0 +1,9 @@
+// [] true
+// {} true
+// {prop: true} false
+// value:string, number, etc return true
+export const isEmpty = (value: unknown): boolean => {
+    const isCountable = value instanceof Object
+    if(!isCountable) return true
+    return Object.entries(value).length === 0
+}
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import viteConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'utils-lib'
+        }
+    },
+    resolve:{
+        alias: {
+            '@src': resolve(__dirname, 'src'),
+            '@types': resolve(__dirname, 'src/types'),
+            // '@utils': resolve(__dirname, '/src/utils')
+        }
+    },
+    plugins: [dts({outDir: 'dist'}), viteConfigPaths()]
+})
