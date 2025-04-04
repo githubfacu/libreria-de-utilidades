d459f9e795024e0dfc45c59f399056429c170b15
githubfacu <elorzfacundo@gmail.com>
2025-04-03 22:14:53 -0300

    fix: throttle imports

--- src/timing/throttle.ts
+++ src/timing/throttle.ts
@@ -1,14 +1,13 @@
+import { AnyFunction } from "@src/types"
 
-type AnyFunction = (...args: unknown[]) => unknown
-
-export function throttle<T extends AnyFunction> (fn: T, delay : number = 500) : (...args: Parameters<T>) => void {
+export function throttle<T extends AnyFunction> (fn:T, delay: number = 500): (...args: Parameters<T>) => void {
     let timeOutId : ReturnType<typeof setTimeout> | undefined = undefined
-
-    return function throttledFunction(...args: Parameters<T>){
-        if (timeOutId !== undefined) {
-            return
+    return function throttledFunction(...args: Parameters<T>) {
+        if(timeOutId !== undefined) {
+            return 
         }
-        timeOutId = setTimeout(() => {
+
+        timeOutId = setTimeout(() => { 
             timeOutId = undefined
         }, delay)
         return fn(...args)
